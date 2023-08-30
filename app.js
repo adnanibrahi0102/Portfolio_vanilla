776b64084c0264a3a793d940226a233d731d7a74
Adnan ibrahim <91428394+adnanibrahi0102@users.noreply.github.com>
2023-08-30 18:46:03 +0530

    feat: Add email sending functionality using EmailJS

--- app.js
+++ app.js
@@ -6,3 +6,25 @@ document.addEventListener('DOMContentLoaded', function() {
         navlinks.classList.toggle('menu-open');
     });
 });
+const ContactForm=document.getElementById('contact')
+const InputName=document.getElementById('name');
+const InputEmail=document.getElementById('email');
+const InputMessage=document.getElementById('message');
+
+const sendEmail=()=>{
+    event.preventDefault();
+  const params={
+    name:InputName.value,
+    email:InputEmail.value,
+    message:InputMessage.value
+  }
+  const serviceID = "service_bsngs6t";
+  const templateID = "template_8awldq9";
+
+  emailjs.send(serviceID,templateID,params).then(res=>{
+    alert("💌 Your email has been successfully sent. Thank you for reaching out!");
+    InputName.value='';
+    InputEmail.value='';
+    InputMessage.value='';
+  }).catch(error=>console.log(error));
+}
