2c2ff01db8aee1311f4616371a1e5ed910ab7ddd
Adnan ibrahim <91428394+adnanibrahi0102@users.noreply.github.com>
2023-08-30 18:49:49 +0530

    refactor - app.js

--- app.js
+++ app.js
@@ -1,30 +1,47 @@
+// When the page content is fully loaded, execute the following code
 document.addEventListener('DOMContentLoaded', function() {
+    // Get the menu toggle and navigation links
     const menuToggle = document.getElementById('menu-toggle');
     const navlinks = document.getElementById('navlinks');
 
+    // Add a click event listener to the menu toggle
     menuToggle.addEventListener('click', function() {
+        // Toggle the "menu-open" class on the navigation links
         navlinks.classList.toggle('menu-open');
     });
 });
-const ContactForm=document.getElementById('contact')
-const InputName=document.getElementById('name');
-const InputEmail=document.getElementById('email');
-const InputMessage=document.getElementById('message');
 
-const sendEmail=()=>{
+// Get the HTML elements for the contact form inputs
+const InputName = document.getElementById('name');
+const InputEmail = document.getElementById('email');
+const InputMessage = document.getElementById('message');
+
+// Function to send the email
+const sendEmail = () => {
+    // Prevent the default form submission behavior
     event.preventDefault();
-  const params={
-    name:InputName.value,
-    email:InputEmail.value,
-    message:InputMessage.value
-  }
-  const serviceID = "service_bsngs6t";
-  const templateID = "template_8awldq9";
 
-  emailjs.send(serviceID,templateID,params).then(res=>{
-    alert("💌 Your email has been successfully sent. Thank you for reaching out!");
-    InputName.value='';
-    InputEmail.value='';
-    InputMessage.value='';
-  }).catch(error=>console.log(error));
-}
+    // Gather the input values into an object
+    const params = {
+        name: InputName.value,
+        email: InputEmail.value,
+        message: InputMessage.value
+    };
+
+    // Define the service and template IDs for EmailJS
+    const serviceID = "service_bsngs6t";
+    const templateID = "template_8awldq9";
+
+    // Send the email using EmailJS
+    emailjs.send(serviceID, templateID, params)
+        .then(res => {
+            // Show a success message with an emoji
+            alert("💌 Your email has been successfully sent. Thank you for reaching out!");
+
+            // Clear the input fields after successful sending
+            InputName.value = '';
+            InputEmail.value = '';
+            InputMessage.value = '';
+        })
+        .catch(error => console.log(error));
+};
